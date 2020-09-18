049439b822920a15572a3c4bbc740d4ba7020291
leviethung123 <levan999mt@gmail.com>
2020-09-18 10:58:28 +0700

    Init Project
    update file moi
    
    Mostly static pages
    
    Chapter 3: Mostly static pages
    
    fix chapter 3->5
    
    fix lần 2 chapter3_5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-	scope "(:locale)", locale: /en|vi/ do
-    resources :microposts
-    resources :users
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/contact"
 end
+
